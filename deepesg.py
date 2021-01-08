e867798044ade2f689e05e1cec6bcfc76ec17a96
Dante-bit <55213931+Dante-bit@users.noreply.github.com>
2021-01-07 23:51:22 -0300

    Update deepesg.py

--- deepesg.py
+++ deepesg.py
@@ -1,8 +1,8 @@
 str = input("-->")
 for i in range(0, int(len(str)/2)): 
         if str[i] != str[len(str)-i-1]:
-            print("Palindromo Palindromo")
+            print("palíndromo")
             break
         else:
-            print("nao palindrono")
+            print("Não palíndromo")
             break
